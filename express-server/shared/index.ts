50700b5a776f437175297fa5c0774c31c6c4cb4c
Rajat Verma <cserajatverma@gmail.com>
2026-02-04 15:31:38 +0530

    Add main export file for shared package
    Create express-server/shared/index.ts as a barrel export to centralize shared module exports. Re-exports './events', './types/common', './constants', './logger', and './redis/client' so other packages can import shared utilities from a single entry point.

---
+++ express-server/shared/index.ts
@@ -0,0 +1,6 @@
+// Main export file for shared package
+export * from './events';
+export * from './types/common';
+export * from './constants';
+export * from './logger';
+export * from './redis/client';
